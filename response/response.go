9f5aa54ed854dc11f4ff142a7acb9ac595cb9d58
takatoh <takatoh.m@gmail.com>
2023-01-18 10:34:56 +0900

    response/response.go(Response): Remove member 'Period' from Response struct.

--- response/response.go
+++ response/response.go
@@ -1,15 +1,13 @@
 package response
 
 type Response struct {
-	Period float64
-	Sa     float64
-	Sv     float64
-	Sd     float64
+	Sa float64
+	Sv float64
+	Sd float64
 }
 
-func NewResponse(period, sa, sv, sd float64) *Response {
+func NewResponse(sa, sv, sd float64) *Response {
 	p := new(Response)
-	p.Period = period
 	p.Sa = sa
 	p.Sv = sv
 	p.Sd = sd
