416aaf416a4f7e5d21f537943540c20ba4d91c18
takatoh <takatoh.m@gmail.com>
2023-01-24 09:03:43 +0900

    directintegration/wilson-theta.go(WilsonTheta): Returns 3 slices of values: acc, vel, dis.

--- directintegration/wilson-theta.go
+++ directintegration/wilson-theta.go
@@ -1,12 +1,10 @@
 package directintegration
 
-import "github.com/takatoh/sdof/response"
-
 const (
 	Theta = 1.4
 )
 
-func WilsonTheta(h, w, dt float64, nn int, ddy []float64) []*response.Response {
+func WilsonTheta(h, w, dt float64, nn int, ddy []float64) ([]float64, []float64, []float64) {
 	tdt := Theta * dt
 	w2 := w * w
 	hw := 2.0 * h * w
@@ -15,21 +13,23 @@ func WilsonTheta(h, w, dt float64, nn int, ddy []float64) []*response.Response {
 	cnume2 := hw * w2 * tdt
 	cnume3 := tdt*hw*tdt/2.0 + w2*tdt*tdt/3.0
 
-	var resp []*response.Response
-	ddx := 0.0
-	dx := 0.0
-	x := 0.0
-	resp = append(resp, response.NewResponse(ddx, dx, x))
+	var ddx []float64
+	var dx []float64
+	var x []float64
+	ddx = append(ddx, 0.0)
+	dx = append(dx, 0.0)
+	x = append(x, 0.0)
 
 	for m := 1; m < nn; m++ {
 		f := (Theta-1.0)*ddy[m] - Theta*ddy[m+1]
-		ath := (f - w2*x - cnume2*dx - cnume3*ddx) / cdenom
-		ddx2 := ((Theta-1.0)*ddx + ath) / Theta
-		dx2 := dx + (ddx+ddx2)*dt/2.0
-		x2 := x + dx*dt + (2.0*ddx+ddx2)*dt*dt/6.0
-		resp = append(resp, response.NewResponse(ddx2+ddy[m], dx2, x2))
-		ddx, dx, x = ddx2, dx2, x2
+		ath := (f - w2*x[m-1] - cnume2*dx[m-1] - cnume3*ddx[m-1]) / cdenom
+		ddx2 := ((Theta-1.0)*ddx[m-1] + ath) / Theta
+		dx2 := dx[m-1] + (ddx[m-1]+ddx2)*dt/2.0
+		x2 := x[m-1] + dx[m-1]*dt + (2.0*ddx[m-1]+ddx2)*dt*dt/6.0
+		ddx = append(ddx, ddx2)
+		dx = append(dx, dx2)
+		x = append(x, x2)
 	}
 
-	return resp
+	return ddx, dx, x
 }
