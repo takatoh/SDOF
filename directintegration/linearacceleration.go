d1d8757e27dfb9bd87a871c9d84027464e4d5b00
takatoh <takatoh.m@gmail.com>
2023-01-28 08:06:11 +0900

    directintegration/linearacceleration.go(LinearAcc): Fix.

--- directintegration/linearacceleration.go
+++ directintegration/linearacceleration.go
@@ -6,19 +6,25 @@ func LinearAcc(h, w, dt float64, nn int, ddy []float64) ([]float64, []float64, [
 	c := 2.0*h*w + w*w*dt
 	d := h*w*dt + (w*w*dt*dt)/3.0
 
-	var ddx, dx, x []float64
+	ddx := make([]float64, 0)
+	dx := make([]float64, 0)
+	x := make([]float64, 0)
 	ddx = append(ddx, -ddy[0])
 	dx = append(dx, 0.0)
 	x = append(x, 0.0)
 
-	var ddx2, dx2, x2 float64
+	var a1, v1, d1 float64
+	a0 := ddx[0]
 	for m := 1; m < nn; m++ {
-		ddx2 = -(ddy[m] + b*x[m-1] + c*dx[m-1] + d*ddx[m-1]) / a
-		dx2 = dx[m-1] + (ddx[m-1]+ddx2)*dt/2.0
-		x2 = x[m-1] + dx[m-1]*dt + (2.0*ddx[m-1]+ddx2)*dt*dt/6.0
-		ddx = append(ddx, ddx2)
-		dx = append(dx, dx2)
-		x = append(x, x2)
+		v0 := dx[m-1]
+		d0 := x[m-1]
+		a1 = -(ddy[m] + b*d0 + c*v0 + d*a0) / a
+		v1 = v0 + (a0+a1)*dt/2.0
+		d1 = d0 + v0*dt + (2.0*a0+a1)*dt*dt/6.0
+		ddx = append(ddx, ddy[m]+a1)
+		dx = append(dx, v1)
+		x = append(x, d1)
+		a0 = a1
 	}
 
 	return ddx, dx, x
