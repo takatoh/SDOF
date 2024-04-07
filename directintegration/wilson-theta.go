a587158303eff74566314810f6726f0d7f81c7e3
takatoh <takatoh.m@gmail.com>
2024-04-07 09:46:32 +0900

    directintegration/wilson-theta.go: Specify size of slices, not use appned().

--- directintegration/wilson-theta.go
+++ directintegration/wilson-theta.go
@@ -13,12 +13,12 @@ func WilsonTheta(h, w, dt float64, nn int, ddy []float64) ([]float64, []float64,
 	cnume2 := hw + w2*tdt
 	cnume3 := hw*tdt/2.0 + w2*tdt*tdt/3.0
 
-	ddx := make([]float64, 0)
-	dx := make([]float64, 0)
-	x := make([]float64, 0)
-	ddx = append(ddx, 0.0)
-	dx = append(dx, 0.0)
-	x = append(x, 0.0)
+	ddx := make([]float64, nn-1)
+	dx := make([]float64, nn-1)
+	x := make([]float64, nn-1)
+	ddx[0] = 0.0
+	dx[0] = 0.0
+	x[0] = 0.0
 
 	a0 := ddx[0]
 	for m := 1; m < nn-1; m++ {
@@ -27,9 +27,9 @@ func WilsonTheta(h, w, dt float64, nn int, ddy []float64) ([]float64, []float64,
 		f := (Theta-1.0)*ddy[m-1] - Theta*ddy[m]
 		ath := (f - w2*d0 - cnume2*v0 - cnume3*a0) / cdenom
 		a1 := ((Theta-1.0)*a0 + ath) / Theta
-		ddx = append(ddx, a1+ddy[m])
-		dx = append(dx, v0+(a0+a1)*dt/2.0)
-		x = append(x, d0+dt*v0+(2.0*a0+a1)*dt*dt/6.0)
+		ddx[m] = a1 + ddy[m]
+		dx[m] = v0 + (a0+a1)*dt/2.0
+		x[m] = d0 + dt*v0 + (2.0*a0+a1)*dt*dt/6.0
 		a0 = a1
 	}
 
