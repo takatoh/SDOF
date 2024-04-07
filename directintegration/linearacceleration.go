f59a96373d224580170459102e1deb047fbbff9f
takatoh <takatoh.m@gmail.com>
2024-04-07 09:50:04 +0900

    directintegration/linearacceleration.go: Specify size of slices, not use appned().

--- directintegration/linearacceleration.go
+++ directintegration/linearacceleration.go
@@ -6,12 +6,12 @@ func LinearAcc(h, w, dt float64, nn int, ddy []float64) ([]float64, []float64, [
 	c := 2.0*h*w + w*w*dt
 	d := h*w*dt + w*w*dt*dt/3.0
 
-	ddx := make([]float64, 0)
-	dx := make([]float64, 0)
-	x := make([]float64, 0)
-	ddx = append(ddx, -ddy[0])
-	dx = append(dx, 0.0)
-	x = append(x, 0.0)
+	ddx := make([]float64, nn)
+	dx := make([]float64, nn)
+	x := make([]float64, nn)
+	ddx[0] = -ddy[0]
+	dx[0] = 0.0
+	x[0] = 0.0
 
 	var a1, v1, d1 float64
 	a0 := ddx[0]
@@ -21,9 +21,9 @@ func LinearAcc(h, w, dt float64, nn int, ddy []float64) ([]float64, []float64, [
 		a1 = -(ddy[m] + b*d0 + c*v0 + d*a0) / a
 		v1 = v0 + (a0+a1)*dt/2.0
 		d1 = d0 + v0*dt + (2.0*a0+a1)*dt*dt/6.0
-		ddx = append(ddx, ddy[m]+a1)
-		dx = append(dx, v1)
-		x = append(x, d1)
+		ddx[m] = ddy[m] + a1
+		dx[m] = v1
+		x[m] = d1
 		a0 = a1
 	}
 
